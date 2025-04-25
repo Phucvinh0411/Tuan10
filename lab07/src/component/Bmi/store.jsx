5e7541611da8420c84c744bc4b2630e3169c7b59
Huynh Nguyen Phuc Vinh <128776680+Phucvinh0411@users.noreply.github.com>
2025-04-25 23:23:37 +0700

    Delete lab07 directory

--- lab07/src/component/Bmi/store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import bmiReducer from './bmiSlice';
-
-export const store = configureStore({
-    reducer: {
-        bmi: bmiReducer,
-    },
-});
